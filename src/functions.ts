50397ff5f4682158e7e8c85a64af1a8cb033afab
acarty324 <acarty324@gmail.com>
2025-09-14 15:52:41 -0400

    commit for hw

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * (5/9)
 }
 
 /**
@@ -12,7 +12,14 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+        let total = 0;
+        if (first>=1) 
+        total += first;
+        if (second >= 1) 
+        total += second;
+        if (third >= 1) 
+        total += third;
+        return total;
 }
 
 /**
@@ -20,7 +27,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +35,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?");
 }
 
 /**
@@ -37,5 +44,15 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
+    const wordUpper = word.toUpperCase();
+    if (wordUpper === "YES"){
+        return true;
+    }else if (wordUpper === "NO"){
+        return false;
+    }else{
+        return null;
+    }
+
+
     return true;
 }
